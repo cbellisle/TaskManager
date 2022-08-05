ccd9535d789039b46acc509473c3019050e5091f
Caleb Bellisle <76009783+cbellisle@users.noreply.github.com>
2022-08-05 14:25:25 -0400

    added password hashing for user safety

--- LoginPage.cs
+++ LoginPage.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,14 +24,20 @@ namespace TaskManager
 
         private void loginButton_Click(object sender, EventArgs e)
         {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
 
-            if (nameBox.TextLength < 1)
-                MessageBox.Show("Please enter a name!");
+
+
+            if (nameBox.TextLength < 1 || passwordBox.TextLength < 1)
+                MessageBox.Show("Please enter a name and password!");
             else
             {
                 //create files and path
                 currentUser = nameBox.Text;
                 string path = $"C:/Users/1259809181117005/source/repos/TaskManager/bin/Debug/users/{currentUser}/{currentUser}.txt";
+                string passwordPath = $"C:/Users/1259809181117005/source/repos/TaskManager/bin/Debug/users/{currentUser}/{currentUser}pw.txt";
+
                 Directory.CreateDirectory($"C:/Users/1259809181117005/source/repos/TaskManager/bin/Debug/users/{currentUser}/");
                 
 
@@ -60,8 +67,30 @@ namespace TaskManager
                             writer.WriteLine(currentTasks);
 
                             writer.Close();
-                            loggedIn(currentUser, currentTasks);
+                            
 
+                        }
+                        using (FileStream text = new FileStream(passwordPath, FileMode.Create))
+                        {
+                            
+                            text.Close();
+
+
+                            var pbkdf2 = new Rfc2898DeriveBytes(passwordBox.Text, salt, 100000);
+                            byte[] hash = pbkdf2.GetBytes(20);
+
+                            byte[] hashBytes = new byte[36];
+                            Array.Copy(salt, 0, hashBytes, 0, 16);
+                            Array.Copy(hash, 0, hashBytes, 16, 20);
+                            StreamWriter writer = new StreamWriter(passwordPath);
+
+                            // add necessary data to user file, name, date tasks
+                            
+                            writer.WriteLine(Convert.ToBase64String(hashBytes));
+
+                            writer.Close();
+
+                            loggedIn(currentUser, currentTasks);
 
                         }
 
@@ -70,13 +99,56 @@ namespace TaskManager
                 }
                 else
                 {
-                    
+
+                    //check user
+                    //check password
+
+
                     StreamReader reader = new StreamReader(path);
-                    reader.ReadLine();//skip first line
-                    reader.ReadLine();//skip second line
-                    Int32.TryParse(reader.ReadLine(), out currentTasks);
-                    reader.Close();
-                    loggedIn(currentUser, currentTasks);
+                    StreamReader password = new StreamReader(passwordPath);
+
+                    if (nameBox.Text == reader.ReadLine())
+                    {
+                        string savedPw = password.ReadLine();
+                        byte[] hashBytes = Convert.FromBase64String(savedPw);
+
+                        /* Get the salt */
+                        byte[] salty = new byte[16];
+                        Array.Copy(hashBytes, 0, salty, 0, 16);
+
+                        /* Compute the hash on the password the user entered */
+                        var pbkdf2 = new Rfc2898DeriveBytes(passwordBox.Text, salty, 10000);
+                        byte[] hash = pbkdf2.GetBytes(20);
+
+                        /* Compare the results */
+
+                        for (int i = 0; i < 20; i++)
+                            if (hashBytes[i + 16] != hash[i])
+                            {
+                                MessageBox.Show("Incorrect login information");
+                                break;
+                            }
+                            else
+                            {
+
+
+                                reader.ReadLine();//skip second line
+                                Int32.TryParse(reader.ReadLine(), out currentTasks);
+                                reader.Close();
+                                loggedIn(currentUser, currentTasks);
+
+                            }
+                    }
+                      
+
+                    //if (nameBox.Text == reader.ReadLine() && passwordBox.Text == password.ReadLine())
+                    //{
+                        
+                    //}
+                    //else
+                    //    MessageBox.Show("Incorrect login");
+                    //reader.ReadLine();//skip first line
+                    
 
 
                     //we dont need to create user files if they already exist so lets just go right in
